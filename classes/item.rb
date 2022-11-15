412df2f252aff0390ab4da4b0a0ab5c449e9d9e1
techEmprez <choiceosobor@gmail.com>
2022-11-15 09:55:05 +0100

    fix linters errors

--- classes/item.rb
+++ classes/item.rb
@@ -1,19 +1,21 @@
 require 'date'
 
 class Item
-    attr_accessor :id, :publish_date, :archived, :genre, :author, :label, :source
-    def initialize(publish_date)
-        @id=Random.rand(1..100)
-        @publish_date=publish_date
-        @archived=false
-    end
+  attr_accessor :id, :publish_date, :archived, :genre, :author, :label, :source
 
-    def move_to_archive
-        @archived= true if can_be_archived?
-    end
+  def initialize(publish_date)
+    @id = Random.rand(1..100)
+    @publish_date = publish_date
+    @archived = false
+  end
 
-    private
-     def can_be_archived?
-        true if(Date.today - Date.parse(@publish_date) > 3650)
-     end
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  private
+
+  def can_be_archived?
+    true if Date.today - Date.parse(@publish_date) > 3650
+  end
 end
