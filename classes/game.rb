1298b0592799d667a5237a312c51bc2d9b2cf00c
techEmprez <choiceosobor@gmail.com>
2022-11-15 16:51:37 +0100

    fix linters issues

--- classes/game.rb
+++ classes/game.rb
@@ -2,16 +2,15 @@ require 'date'
 require_relative 'item'
 
 class Game < Item
-    attr_accessor :multiplayer, :last_played_at
+  attr_accessor :multiplayer, :last_played_at
 
-    def initialize(publish_date, multiplayer, last_played_at)
-        super(publish_date)
-        @multiplayer = multiplayer
-        @last_played_at = last_played_at
-    end
-    
-    def can_be_archived?
-        super && Date.today - Date.parse(@last_played_at) > 730
-    end
-    end
-      
+  def initialize(publish_date, multiplayer, last_played_at)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    super && Date.today - Date.parse(@last_played_at) > 730
+  end
+end
