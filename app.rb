adc59088577232ef4a979c7b77cc7ffccb44e6b0
techEmprez <choiceosobor@gmail.com>
2022-11-16 13:43:51 +0100

    modify validation path

---
+++ app.rb
@@ -0,0 +1,13 @@
+require_relative 'classes/item'
+require_relative 'classes/game'
+require_relative 'classes/author'
+require_relative 'classes/music'
+require_relative 'classes/genre'
+require_relative 'classes/movie'
+require_relative 'classes/source'
+require_relative 'validation'
+require_relative 'handler/handle_label'
+require_relative 'handler/handle_genre'
+require_relative 'handler/handle_author'
+
+require 'json'
