a9a53e77c1f803c31d7d088920df63f513197db7
techEmprez <choiceosobor@gmail.com>
2022-11-16 14:34:06 +0100

    create list methods in app.rb

--- app.rb
+++ app.rb
@@ -17,3 +17,26 @@ class App
 
     load_author
   end
+
+  def menu_options
+    puts 'WELCOME TO MY CATALOG OF THINGS!'
+    while @response != 13
+      puts(
+        'Please choose an option by entering a number:
+        1 - List all books
+        2 - List all music albums
+        3 - List of games
+        4 - List all movies
+        5 - List all labels
+        6 - List all genres
+        7 - List all authors
+        8 - List all sources
+        9 - Add a book
+        10 - Add a music album
+        11 - Add a game
+        12 - Add a movie
+        13 - Exit'
+      )
+      start_app
+    end
+  end
