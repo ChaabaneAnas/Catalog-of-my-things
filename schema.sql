39fae6e3c1396493c059bb7524ca2ba2e801aa05
Anas Chaabane <Anas.chaabane98@gmail.com>
2022-11-16 18:54:24 +0100

    schema conflict

--- schema.sql
+++ schema.sql
@@ -23,3 +23,26 @@ CREATE TABLE books (
   cover_state VARCHAR(100),
   FOREIGN KEY(id) REFERENCES item(id)
 );
+
+CREATE TABLE authors (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE games(
+  id SERIAL PRIMARY KEY,
+  type_of_item text
+  label_id INTEGER,
+  genre_id INTEGER,
+  author_id INTEGER,
+  source_id INTEGER,
+  publish_date DATE,
+  archived BOOLEAN,
+  MULTIPLAYER TEXT,
+  LAST_PLAYED_AT DATE,
+  FOREIGN KEY (label_id) REFERENCES labels(id),
+  FOREIGN KEY (genre_id) REFERENCES genres(id),
+  FOREIGN KEY (author_id) REFERENCES authors(id),
+  FOREIGN KEY (source_id) REFERENCES sources(id)
+);
